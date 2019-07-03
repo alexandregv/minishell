e4ce91c853a440b72a2225c1a90207d46be8f205
aguiot-- <aguiot--@student.42.fr>
2019-07-03 16:11:08 +0200

    Handle cd - (oldpwd)

--- src/cd_builtin.c
+++ src/cd_builtin.c
@@ -6,7 +6,7 @@
 /*   By: aguiot-- <aguiot--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 08:55:04 by aguiot--          #+#    #+#             */
-/*   Updated: 2019/07/03 11:04:21 by aguiot--         ###   ########.fr       */
+/*   Updated: 2019/07/03 16:10:12 by aguiot--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	cd_builtin(char **argv, char **env)
 {
 	char	*str;
 	char	*err;
+	char	*old;
 	int		ret;
 
 	if ((str = ft_join_table(argv + 1, " ")) == NULL)
@@ -27,6 +28,13 @@ int	cd_builtin(char **argv, char **env)
 		if (!(str = ft_strdup(ft_getenv(env, "HOME"))))
 			exit(1); //TODO: Bonus /home/getpwuid() ?
 	}
+	else if (ft_strlen(str) == 1 && !ft_strcmp(str, "-"))
+	{
+		free(str);
+		if (!(str = ft_strdup(ft_getenv(env, "OLDPWD"))))
+			exit(1);
+	}
+	old = getcwd(NULL, 255);
 	ret = chdir(str);
 	if (ret == -1)
 	{
@@ -37,6 +45,9 @@ int	cd_builtin(char **argv, char **env)
 		ft_putendl_fd(err, 2);
 		free(err);
 	}
+	else
+		env = ft_setenv(env, "OLDPWD", old);
+	free(old);
 	free(str);
 	return (ret);
 }
