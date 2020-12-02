b082dc1ab1733a25588902d68f774b6b9f810751
aguiot-- <aguiot--@student.42.fr>
2020-12-02 09:15:36 +0100

    WIP: Handle multiple expansions

--- src/expansions.c
+++ src/expansions.c
@@ -6,11 +6,20 @@
 /*   By: aguiot-- <aguiot--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/28 16:52:29 by aguiot--          #+#    #+#             */
-/*   Updated: 2020/12/02 03:21:48 by aguiot--         ###   ########.fr       */
+/*   Updated: 2020/12/02 03:56:35 by aguiot--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static char	*chr(char const *s, int c)
+{
+	while (*s && (*s != c || (*s == c && *(s + 1) == c)))
+		++s;
+	if (*s != c)
+		return (NULL);
+	return ((char*)s);
+}
 
 static int	is_alnum_or_questionmark(char c)
 {
@@ -22,7 +31,7 @@ static char	*tilde_expansion(char *cmd, char **env)
 	char	*tmp;
 	char	*val;
 
-	if (ft_strchr(cmd, '~'))
+	while (ft_strchr(cmd, '~') != NULL)
 	{
 		tmp = cmd;
 		val = ft_getenv(env, "HOME");
@@ -40,20 +49,23 @@ static char	*var_expansion(char *cmd, char **env)
 	char	*var;
 	char	*val;
 
-	dol = ft_strchr(cmd, '$');
-	if (dol && *(dol + 1) != '$' && *(dol + 1) != '\0')
+	while ((dol = chr(cmd, '$')) != NULL)
 	{
-		tmp1 = cmd;
-		ft_str_copy_while(&var, ft_strchr(cmd, '$') + 1,
-			&is_alnum_or_questionmark);
-		val = ft_getenv(env, var);
-		tmp2 = var;
-		var = ft_strjoinfree(ft_strdup("$"), var, ft_strlen(var));
-		cmd = ft_strreplace(cmd, var, val ? val : "");
-		free(var);
-		free(tmp1);
-		free(tmp2);
+		if (dol && *(dol + 1) != '$' && *(dol + 1) != '\0')
+		{
+			tmp1 = cmd;
+			ft_str_copy_while(&var, chr(cmd, '$') + 1,
+					&is_alnum_or_questionmark);
+			val = ft_getenv(env, var);
+			tmp2 = var;
+			var = ft_strjoinfree(ft_strdup("$"), var, ft_strlen(var));
+			cmd = ft_strreplace(cmd, var, val ? val : "");
+			free(var);
+			free(tmp1);
+			free(tmp2);
+		}
 	}
+ft_putendl("end var");
 	return (cmd);
 }
 
@@ -63,8 +75,7 @@ static char	*dollar_expansion(char *cmd, char **env)
 	char	*tmp;
 	char	*pid;
 
-	dol = ft_strchr(cmd, '$');
-	if (dol && *(dol + 1) == '$')
+	while (1==2 && (dol = chr(cmd, '$')) != NULL)
 	{
 		pid = ft_itoa((int)getpid());
 		tmp = cmd;
